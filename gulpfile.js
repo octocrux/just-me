2572465a2d68b2b4ba78487a484192dbb31168bb
octocrux <cate.petrusevich@gmail.com>
2016-03-18 15:52:02 +0300

    Add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var stylus = require('gulp-stylus');
+var browserSync = require('browser-sync');
 
 gulp.task('html', function () {
   return gulp.src('index.jade')
@@ -12,4 +13,16 @@ gulp.task('css', function () {
   return gulp.src('index.styl')
     .pipe(stylus())
     .pipe(gulp.dest('./build'));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './build'
+    }
+  });
+
+  gulp.watch('index.jade', ['html']);
+  gulp.watch('index.styl', ['css']);
+  gulp.watch('./build/*', browserSync.reload);
 });
