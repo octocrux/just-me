dfd89d6f51d58294ee6e54036e057649681bcdef
octocrux <cate.petrusevich@gmail.com>
2016-03-18 15:30:00 +0300

    Add stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
 
 gulp.task('html', function () {
-    return gulp.src('index.jade')
-        .pipe(jade())
-        .pipe(gulp.dest('./build'));
+  return gulp.src('index.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('css', function () {
+  return gulp.src('index.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./build'));
 });
